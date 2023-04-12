ca5ae280d61572d7282ef1688c6145f9bda3b675
sreedhar-98 <sreedhardronamraju1@gmail.com>
2023-04-12 15:43:56 +0530

    Modified transform class

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -10,6 +10,7 @@ import pandas as pd
 import sys
 import numpy as np
 from src.utils import save_object
+import json
 
 @dataclass
 class TransformationConfig:
@@ -22,11 +23,24 @@ class DataTransformer:
         try:
             #Categorical Feature Encoding
             # Define the custom ranking for each ordinal variable
-            cut_categories = ['Fair', 'Good', 'Very Good','Premium','Ideal']
-            color_categories = ['D', 'E', 'F', 'G', 'H', 'I', 'J']
-            clarity_categories = ['I1','SI2','SI1','VS2','VS1','VVS2','VVS1','IF']
-            categorical_cols = ['cut', 'color','clarity']
-            numerical_cols = ['carat', 'depth','table', 'x', 'y', 'z']
+            # cut_categories = ['Fair', 'Good', 'Very Good','Premium','Ideal']
+            # color_categories = ['D', 'E', 'F', 'G', 'H', 'I', 'J']
+            # clarity_categories = ['I1','SI2','SI1','VS2','VS1','VVS2','VVS1','IF']
+            # categorical_cols = ['cut', 'color','clarity']
+            # numerical_cols = ['carat', 'depth','table', 'x', 'y', 'z']
+
+            with open('src/feature_engineering_artifacts/features_dict.json','r') as f:
+                cols=json.load(f)
+            with open('src/feature_engineering_artifacts/encoding.json','r') as f:
+                encoding=json.load(f)
+            
+            numerical_cols=cols['numerical_cols']
+            categorical_cols=cols['categorical_cols']
+
+            cut_categories=encoding['cut']
+            color_categories=encoding['color']
+            clarity_categories=encoding['clarity']
+            
             num_pipeline=Pipeline( 
                 steps=[  
                 ('imputer',SimpleImputer(strategy='median')),
@@ -53,11 +67,20 @@ class DataTransformer:
         try:
             train_data=pd.read_csv(train_data_path)
             test_data=pd.read_csv(test_data_path)
-            X_train=train_data.drop('price',axis=1)
-            y_train=train_data['price']
+            
+            with open('src/feature_engineering_artifacts/features_dict.json','r') as f:
+                cols=json.load(f)
+            dropped_cols=cols['dropped_cols']
+            feature_col=cols['target_col']
 
-            X_test=test_data.drop('price',axis=1)
-            y_test=test_data['price']
+            train_data.drop(dropped_cols,axis=1,inplace=True)
+            test_data.drop(dropped_cols,axis=1,inplace=True)
+            
+            X_train=train_data.drop(feature_col,axis=1)
+            y_train=train_data[feature_col]
+
+            X_test=test_data.drop(feature_col,axis=1)
+            y_test=test_data[feature_col]
 
             preprocessor=self.get_transform_object()
             logging.info("Applying preprocessing")
